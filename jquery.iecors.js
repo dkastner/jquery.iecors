c636f71fdf9db2c078118d7118752a1f176fe0a0
Neogavin <Neogavin@gmail.com>
2012-03-28 15:42:24 -0700

    update jquery.iecors.js to prevent hangup in IE browsers when resource is not found.

--- jquery.iecors.js
+++ jquery.iecors.js
@@ -2,46 +2,46 @@
   // Create the request object
   // (This is still attached to ajaxSettings for backward compatibility)
   jQuery.ajaxSettings.xdr = function() {
-    return (window.XDomainRequest ? new window.XDomainRequest() : null);
+	return (window.XDomainRequest ? new window.XDomainRequest() : null);
   };
 
   // Determine support properties
   (function( xdr ) {
-    jQuery.extend( jQuery.support, { iecors: !!xdr });
+	jQuery.extend( jQuery.support, { iecors: !!xdr });
   })( jQuery.ajaxSettings.xdr() );
 
   // Create transport if the browser can provide an xdr
-  if ( jQuery.support.iecors ) {
+	if ( jQuery.support.iecors ) {
+ 	    jQuery.ajaxTransport(function( s ) {
+		return {
+			send: function( headers, complete ) {
+			  var xdr = s.xdr();
 
-    jQuery.ajaxTransport(function( s ) {
-      var callback;
+			  xdr.onload = function() {
+				var headers = { 'Content-Type': xdr.contentType };
+				complete(200, 'OK', { text: xdr.responseText }, headers);
+			  };
+			  
+			  // Apply custom fields if provided
+			  if(s.xhrFields) {
+				xdr.onerror = s.xhrFields.error;
+				xdr.ontimeout = s.xhrFields.timeout;
+			  } else {
+				//Needed to prevent hangup when not defined in IE
+				xdr.onerror = function() {complete(404, 'Not Found')}
+				xdr.ontimeout = function() {complete(408, 'Request Time-Out')}
+			  }
+			  xdr.timeout = 5000;
+			  xdr.open( s.type, s.url );
 
-      return {
-        send: function( headers, complete ) {
-          var xdr = s.xdr();
+			  // XDR has no method for setting headers O_o
 
-          xdr.onload = function() {
-            var headers = { 'Content-Type': xdr.contentType };
-            complete(200, 'OK', { text: xdr.responseText }, headers);
-          };
-          
-          // Apply custom fields if provided
-					if ( s.xhrFields ) {
-            xhr.onerror = s.xhrFields.error;
-            xhr.ontimeout = s.xhrFields.timeout;
-					}
+			  xdr.send( ( s.hasContent && s.data ) || null );
+			},
 
-          xdr.open( s.type, s.url );
+			abort: s.xdr().abort
 
-          // XDR has no method for setting headers O_o
-
-          xdr.send( ( s.hasContent && s.data ) || null );
-        },
-
-        abort: function() {
-          xdr.abort();
-        }
-      };
-    });
-  }
+		};
+	     });
+	}
 })( jQuery );
